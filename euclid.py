1510830f3f385029a3130021dc921879fc4de869
Heyon <piaohuijuan@gmail.com>
2024-04-29 10:50:32 +0900

    制御構文の課題をコミットします。

--- euclid.py
+++ euclid.py
@@ -2,3 +2,9 @@ a = input("a の値を入力: ")
 b = input("b の値を入力: ")
 
 # TODO
+#問3
+while b != 0:
+    gcd = b
+    (a, b) = (b, a % b)
+    print(gcd)
+    
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,24 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+#問2
+from math import sin
+
+# 区間の上限と下限
+a = 0
+b = 3.141592653589793 / 2  # π/2
+
+# 分割数
+n = 100
+
+# 分割幅
+delta_x = (b - a) / n
+
+# 台形積分の計算
+integral = 0.5 * delta_x * (sin(a) + sin(b))
+
+for i in range(1, n):
+    integral += delta_x * sin(a + i * delta_x)
+
+print("積分値の近似値:", integral)
+
