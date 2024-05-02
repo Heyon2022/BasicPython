168499be0a3fddd490d994beaece47ab8c51f0c0
Heyon <piaohuijuan@gmail.com>
2024-05-03 04:39:02 +0900

    関数問1をコミットします。

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,11 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+a = int(input("aの値を入力: "))
 
 # TODO
+def is_prime(a):
+    if a <= 1:
+        return False
+    for x in range(2,a):
+        if a%x == 0:
+            return False
+    return True
+print(is_prime(a))
