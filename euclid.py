d5842def2035dc22b05337596753742d3962c866
Heyon <piaohuijuan@gmail.com>
2024-05-14 22:14:29 +0900

    関数課題をコミットします。

--- euclid.py
+++ euclid.py
@@ -2,8 +2,19 @@ a = int(input("a の値を入力: "))
 b = int(input("b の値を入力: "))
 
 # TODO
-#問3
-while b != 0:
-    (a, b) = (b, a % b)
-print(a)
+#最大公約数の計算
+def gcd(a, b):
+    while b != 0:
+        (a, b) = (b, a % b)
+    return a
+print(gcd(a,b))
+
+#素数判定
+def are_coprime(a, b):
+    if gcd(a, b) == 1:
+        return "aとbは互いに素です。"
+    else:
+        return "aとbは互いに素ではありません。"
+print(are_coprime(a, b))
+
     
--- prime_number.py
+++ prime_number.py
@@ -1,14 +1,23 @@
 a = int(input("aの値を入力: "))
 
 # TODO
-#問1
-if a <=1:
-    print(False)
+try:
+    a = int(input("aの値を入力: "))
+    if a <= 0:
+        raise ValueError("自然数を入力してください。")
+except ValueError as message:
+    message = "自然数を入力してください。"
+    print(message)
+    exit()
 else:
-    for i in range(2,int(a**0.5)+1):
-        if a % i == 0:
-            print(False)
-            break
-    else:
-      print(True)
+    # 自然数が入力された場合にのみ、is_prime 関数を実行
+    def is_prime(a):
+        if a == 1:
+            return False
+        for x in range(2, a):
+                return False
+        return True
+    
+    print(is_prime(a))
+    
 
